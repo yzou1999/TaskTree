99bb2dc04e8944938f083a8667be6cc980bd2b95
yzou1999 <yzou1999@gmail.com>
2021-10-19 15:28:32 -0400

    register page form

--- frontend/src/components/loginPage.js
+++ frontend/src/components/loginPage.js
@@ -3,12 +3,12 @@ import React from "react";
 function login() {
     return (
         <div className = "loginContainer">
-            <h1 align="middle"> Login Form </h1> 
-            <form align="middle" method="post" action="/login_form">
-                <label>Username: </label>
+            <h1 align = "middle"> Login Form </h1> 
+            <form align = "middle" method = "post" action = "/login_form">
+                <label>Username:  </label>
                 <input name = "user_log" type = "text" placeholder = "username12345"></input>
                 <br/><br/>
-                <label>Password: </label>
+                <label>Password:  </label>
                 <input name = "user_pwd" type = "password"></input>
                 <br/><br/>
                 <input type = "submit" value = "enter"></input>
