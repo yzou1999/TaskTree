d4d736ec9a95f6f5bc9e211e75e748576d16f20d
yzou1999 <yzou1999@gmail.com>
2021-10-21 15:03:10 -0400

    connected to DB

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,7 +10,7 @@ router.route('/').get((req,res) => {
 router.route('/add').post((req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    const newUser = new User({username, password});
+    const newUser = new User({username,password});
     newUser.save()
         .then(() => res.json("user added"))
         .catch(err => res.status(400).json("error"));
