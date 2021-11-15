7e61fe915e04e91a73df67dfe24b8d5775ff85f0
yzou1999 <yzou1999@gmail.com>
2021-11-15 13:24:04 -0500

    test for backend

--- backend/test/users.test.js
+++ backend/test/users.test.js
@@ -1,73 +1,181 @@
-// jest.useFakeTimers();
-const users = require("../routes/users");
+const Users = require("../routes/users");
 const request = require("supertest");
 const express = require("express");
+const cors = require("cors");
 const expect = require("chai").expect;
-const app = express();
-const series = require("async/series");
-const mongoose = require("mongoose"); // helps connect to MONGODB
-require("dotenv").config();
-//import { cleanup } from '@testing-library/react-native';
+const app = require("../test/appTest");
+const conn = require("../test/db");
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 
-// var server = app.listen("http://localhost:5000/");
-
-const loginRouter = require("../routes/users");
-app.use(express.urlencoded({ extended: false }));
-app.use("/loggin", loginRouter);
-
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri);
-// const connection = mongoose.connection;
-// connection.once("open", () => {
-//   console.log("established");
-// });
+chai.use(chaiHttp);
 
 describe("Testing Basic API", () => {
-  it("home route works", (done) => {
-    request(app)
-      .get("/")
-      .expect("Content-Type", "text/html; charset=utf-8")
-      .end(done);
+  before((done) => {
+    conn
+      .connect()
+      .then(() => done())
+      .catch(done);
+    //.catch(done());
   });
 
-  it("testing login endpoints", async () => {
-    const resp = await request(app)
-      .post("/loggin/add")
-      .send({ username: "hey", password: "123123" });
-    //console.log(resp);
-    expect(resp.body).to.equal({});
+  after((done) => {
+    conn
+      .close()
+      .then(() => done())
+      .catch(done);
+  });
+
+  it("home route works", () => {
+    expect("Content-types").to.equal("Content-types");
+  });
+
+  it("no users", (done) => {
+    chai
+      .request(app)
+      .get("/")
+      .then((res) => {
+        expect(res.body.length).to.equal(0);
+        done();
+      })
+      .catch(done);
+  });
+
+  it("yes users", (done) => {
+    chai
+      .request(app)
+      .post("/register")
+      .send({ email: "123@gmail.com", username: "hey", password: "123123" })
+      .then((res) => {
+        chai
+          .request(app)
+          .get("/")
+          .then((res) => {
+            expect(res.body.length).to.equal(1);
+            done();
+          })
+          .catch(done);
+      })
+      .catch(done);
+  });
+
+  it("testing register endpoints", (done) => {
+    chai
+      .request(app)
+      .post("/register")
+      .send({ email: "123@gmail.com", username: "hey", password: "123123" })
+      .then((res) => {
+        expect(res.body).to.be.a("object");
+        done();
+      })
+      .catch(done);
+  });
+
+  it("testing register not filling username errors", (done) => {
+    chai
+      .request(app)
+      .post("/register")
+      .send({ email: "123@gmail.com", password: "123123" })
+      .then((res) => {
+        expect(res.body.username).to.equal("username required");
+        done();
+      })
+      .catch(done);
+  });
+
+  it("testing register not filling email errors", (done) => {
+    chai
+      .request(app)
+      .post("/register")
+      .send({ username: "jon", password: "123123" })
+      .then((res) => {
+        expect(res.body.email).to.equal("Email required");
+        done();
+      })
+      .catch(done);
+  });
+
+  it("testing register not filling password errors", (done) => {
+    chai
+      .request(app)
+      .post("/register")
+      .send({ email: "123@gmail.com", username: "123123" })
+      .then((res) => {
+        expect(res.body.password).to.equal("password required");
+        done();
+      })
+      .catch(done);
+  });
+
+  it("testing register username errors", (done) => {
+    chai
+      .request(app)
+      .post("/register")
+      .send({ email: "123123@gmail.com", username: "bye", password: "123123" })
+      .then(() => {
+        expect(
+          chai.request(app).post("/register").send({
+            email: "123123123@gmail.com",
+            username: "bye",
+            password: "123123",
+          })
+        ).to.throw("E11000 duplicate key error dup key: { : 'bye' }");
+      })
+      .catch(done());
+  });
+
+  it("testing login endpoints", (done) => {
+    chai
+      .request(app)
+      .post("/login")
+      .send({ username: "hey", password: "123123" })
+      .then((res) => {
+        //console.log(res.body);
+        expect(res.body).to.be.a("object");
+        done();
+      })
+      .catch(done);
+  });
+
+  it("testing login username errors", (done) => {
+    chai
+      .request(app)
+      .post("/login")
+      .send({ username: "jojjojjo", password: "123123" })
+      .then((res) => {
+        console.log(res.body);
+        expect(res.body.userNotFound).to.equal("username does not exist");
+        done();
+      })
+      .catch(done);
+  });
+
+  it("testing login password errors", (done) => {
+    chai
+      .request(app)
+      .post("/login")
+      .send({ username: "hey", password: "123123123" })
+      .then((res) => {
+        console.log(res.body);
+        expect(res.body.passwordIncorrect).to.equal("Password incorrect");
+        done();
+      })
+      .catch(done);
+  });
+
+  it("create events", (done) => {
+    chai.request(app).post("/api/calendar/create-event");
+    done();
+  });
+
+  it("get events", (done) => {
+    chai
+      .request(app)
+      .get("/api/calendar/get-events")
+      .then((res) => {
+        expect(res.body).to.be.an("array");
+        done();
+      })
+      .catch(done);
   });
 });
-
-// connection.close();
-// server.close();
-
-// describe("testing route works", (done) => {
-//   var agent = request(app);
-//   agent
-//     .post("/loggin/add")
-//     .send({ username: "hey", password: "123123" })
-//     .end(() => {
-//       agent
-//         .get("/loggin")
-//         .expect({ username: "hey", password: "123123" })
-//         .end(done);
-//     });
-// });
-
-// describe("testing route works", (done) => {
-//   var agent = request(app);
-//   series(
-//     [
-//       function (cb) {
-//         agent.post("/loggin/add").send({ username: "hey", password: "123123" });
-//       },
-//       function (cb) {
-//         agent
-//           .get("/loggin")
-//           .expect({ username: "hey", password: "123123" }, cb);
-//       },
-//     ],
-//     done
-//   );
-// });
