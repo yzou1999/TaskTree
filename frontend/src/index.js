5a520bb1224f55ad6b3991b40d887f7324566835
alexandrajaccard <alexandra.fong23@gmail.com>
2021-11-17 15:32:57 -0500

    dashboard

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,9 @@ import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+<App />
+  </>,
   document.getElementById('root')
 );
 
